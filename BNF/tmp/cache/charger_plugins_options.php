d80104d107b1e10898033125df9f3e1b3f573b50
Cedricldc <dercic44@hotmail.fr>
2017-04-07 15:53:51 +0200

    modif template

--- BNF/tmp/cache/charger_plugins_options.php
+++ BNF/tmp/cache/charger_plugins_options.php
@@ -1,7 +1,7 @@
 <?php
 
 if (defined('_ECRIRE_INC_VERSION')) {
-define('_PLUGINS_HASH','f85d14cff6b4d67139baba97f2aff3fb');
+define('_PLUGINS_HASH','d80ff6c97517ee5cddff2a23ff32a5a9');
 include_once_check(_ROOT_PLUGINS_DIST.'petitions/petitions_options.php');
 include_once_check(_ROOT_PLUGINS_DIST.'squelettes_par_rubrique/squelettes_par_rubrique_options.php');
 include_once_check(_ROOT_PLUGINS.'auto/skeleditor/v2.7.11/skeleditor_options.php');
---
+++ BNF/tmp/sessions/1_2c72e376d3a11cc4388894f265d694e8.php
@@ -0,0 +1,27 @@
+<?php
+$GLOBALS['visiteur_session']['id_auteur'] = 1;
+$GLOBALS['visiteur_session']['nom'] = 'Cedricldc';
+$GLOBALS['visiteur_session']['bio'] = '';
+$GLOBALS['visiteur_session']['email'] = 'cedric.leduc@icloud.com';
+$GLOBALS['visiteur_session']['nom_site'] = '';
+$GLOBALS['visiteur_session']['url_site'] = '';
+$GLOBALS['visiteur_session']['login'] = 'Cedricldc';
+$GLOBALS['visiteur_session']['statut'] = '0minirezo';
+$GLOBALS['visiteur_session']['webmestre'] = 'oui';
+$GLOBALS['visiteur_session']['maj'] = '2017-04-07 15:48:58';
+$GLOBALS['visiteur_session']['pgp'] = '';
+$GLOBALS['visiteur_session']['en_ligne'] = '2017-04-07 14:48:27';
+$GLOBALS['visiteur_session']['prefs'] = array (
+  'couleur' => 9,
+  'display' => 2,
+  'display_navigation' => 'navigation_avec_icones',
+  'display_outils' => 'oui',
+  'cnx' => '',
+);
+$GLOBALS['visiteur_session']['source'] = 'spip';
+$GLOBALS['visiteur_session']['lang'] = '';
+$GLOBALS['visiteur_session']['auth'] = 'spip';
+$GLOBALS['visiteur_session']['hash_env'] = 'd2acc4acd6f357ae6f9a83bdf618a869';
+$GLOBALS['visiteur_session']['ip_change'] = false;
+$GLOBALS['visiteur_session']['date_session'] = 1491572938;
+?>
